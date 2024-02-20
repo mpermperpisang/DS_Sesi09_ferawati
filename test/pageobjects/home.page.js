87d5b490f52a2eb776018eea73919c0c3206b6a8
Ferawati Hartanti Pratiwi <mpermperpisang@gmail.com>
2024-02-20 21:03:00 +0700

    fix: remove unecessary wording

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -15,12 +15,3 @@ class HomePage extends Page {
 }
 
 module.exports = new HomePage();
-
-1. npm init wdio . -- --yes // NOTE: auto install default setup webdriverio
-2. buat scenario di folder specs, berupa describe dan it
-3. buat function kosongan di scenario specs dengan dummy page
-4. buat page object untuk page-page yang ingin dibuatkan scenario (contoh: login & home page)
-5. buat variable name untuk element kosongan
-6. buat function kosongan untuk di page object yang diinginkan
-7. inspect element dengan cara click kanan di browser, kemudian search element apakah ada atau tidak
-8. panggil element di function-function kosong yang sudah ditentukan (harus step by step)
